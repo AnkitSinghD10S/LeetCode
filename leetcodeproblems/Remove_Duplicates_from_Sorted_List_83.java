c9a0aaedbfb366c788222486a4de75167738fec8
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-02-04 19:02:33 +0530

    Remove_Duplicates_from_Sorted_List_83

--- leetcodeproblems/Remove_Duplicates_from_Sorted_List_83.java
+++ leetcodeproblems/Remove_Duplicates_from_Sorted_List_83.java
@@ -1,15 +1,24 @@
 public class Remove_Duplicates_from_Sorted_List_83 {
-    class Solution {
-        public ListNode deleteDuplicates(ListNode head) {
-            ListNode node= head;
-            while(node!=null && node.next!= null){
-                    if(node.val==node.next.val){
-                    node.next=node.next.next;
-                    }else{
-                        node=node.next;
-                    }
-            }
-        return head;
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode node= head;
+        while(node!=null && node.next!= null){
+                if(node.val==node.next.val){
+                node.next=node.next.next;
+                }else{
+                    node=node.next;
+                }
         }
+            return head;
+        }
+    public class ListNode{
+        ListNode next;
+        int val;
+        ListNode(int x){
+            this.val=x;
+            ListNode next=null;
+        }
+    }
+    public static void main(String[] args) {
+        
     }
 }
