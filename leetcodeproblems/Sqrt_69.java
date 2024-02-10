abdede38e6c23a0b26e86d4d7fbdc917e0d03417
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-02-10 10:42:55 +0530

    Sqrt_69.java

---
+++ leetcodeproblems/Sqrt_69.java
@@ -0,0 +1,21 @@
+public class Sqrt_69 {
+    public int mySqrt(int x) {
+        long ans = 0;
+        long start = 0;
+        long end = x;
+        while (start <= end) {
+            long mid = start + (end - start) / 2;
+            long square = mid * mid;
+            if (square == x) {
+                ans = (int) mid;
+                break;
+            } else if (square > x)
+                end = mid - 1;
+            else {
+                start = mid + 1;
+                ans = mid;
+            }
+        }
+        return (int) ans;
+    }
+}
