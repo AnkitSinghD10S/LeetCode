2451a1f0179764ee63ed6b65922b1b3d8e019b93
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-02-13 23:57:21 +0530

    Find_First_Palindromic_String_in_the_Array_2108

---
+++ leetcodeproblems/Find_First_Palindromic_String_in_the_Array_2108.java
@@ -0,0 +1,44 @@
+/*Given an array of strings words, return the first palindromic string in the array. If there is no such string, return an empty string "".
+A string is palindromic if it reads the same forward and backward.
+Example 1:
+Input: words = ["abc","car","ada","racecar","cool"]
+Output: "ada"
+Explanation: The first string that is palindromic is "ada".
+Note that "racecar" is also palindromic, but it is not the first.
+Example 2:
+Input: words = ["notapalindrome","racecar"]
+Output: "racecar"
+Explanation: The first and only string that is palindromic is "racecar".
+Example 3:
+Input: words = ["def","ghi"]
+Output: ""
+Explanation: There are no palindromic strings, so the empty string is returned.
+Constraints:
+1 <= words.length <= 100
+1 <= words[i].length <= 100
+words[i] consists only of lowercase English letters.*/
+
+public class Find_First_Palindromic_String_in_the_Array_2108 {
+    public String firstPalindrome(String[] words) {
+        for(int i=0;i<words.length;i++){
+            if(palindrom(words[i])){
+                return words[i];
+            }
+        }
+        return "";
+    }
+    public boolean palindrom(String s){
+        int i=0;
+        int j=s.length()-1;
+        while(i<j){
+            if(s.charAt(i)==s.charAt(j)){
+                i++;
+                j--;
+            }
+            else{
+                return false;
+            }
+        }
+        return true;
+    }
+}
