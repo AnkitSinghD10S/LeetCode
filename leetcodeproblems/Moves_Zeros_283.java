6924ec04f0c01db18d3247c6c2313c1d29cacc72
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-02-28 07:05:44 +0530

    Moves_Zeros_283

--- leetcodeproblems/Moves_Zeros_283.java
+++ leetcodeproblems/Moves_Zeros_283.java
@@ -1,5 +1,14 @@
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+Example 1:
+Input: nums = [0,1,0,3,12]
+Output: [1,3,12,0,0]
+Example 2:
+Input: nums = [0]
+Output: [0] */
 public class Moves_Zeros_283 {
     public static void moveZeroes(int[] nums) {
+        // Usng sorting 
     //     for(int i=0;i<nums.length;i++){
     //         for(int j=i+1;j<nums.length;j++){
     //             if(nums[i]==0){
@@ -10,9 +19,8 @@ public class Moves_Zeros_283 {
     //         System.out.println(nums[i]);
     //     }
     // }
-
-
-    // public static void moveZeroes(int[] nums) {
+ 
+    // // public static void moveZeroes(int[] nums) {
     //     int i = 0;
     //     for(int num:nums){
     //         if(num!=0){
