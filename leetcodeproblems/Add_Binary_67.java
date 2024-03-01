d82fcbe9d1c1ae167324c009dead7c8291c65e4b
AnkitSinghD10S <147189008+AnkitSinghD10S@users.noreply.github.com>
2024-03-01 22:21:10 +0530

    Add_Binary_67

--- leetcodeproblems/Add_Binary_67.java
+++ leetcodeproblems/Add_Binary_67.java
@@ -1,5 +1,11 @@
 /**
- * Add_Binary_67
+Given two binary strings a and b, return their sum as a binary string.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
  */
 public class Add_Binary_67 {
 
